7b3a6693fc5cd3cd225a2dfe44e42e736b6211fa
Venkata Satyanarayana. Alokam <31393949+alokamvenki402@users.noreply.github.com>
2017-09-21 15:25:20 -0400

    Update name.sh

--- name.sh
+++ name.sh
@@ -11,3 +11,8 @@ echo "Total number of arguments is $#"
 echo "My first name is $1"
 echo "My surname is $2"
 echo "Total number of arguments is $#"
+
+
+echo "My first name is $1"
+echo "My surname is $2"
+echo "Total number of arguments is $#"
